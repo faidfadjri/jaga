2a77bd46afc23bcd013f38267bf506aea7270b45
faidfadjri <faidfadjri.busdev@gmail.com>
2024-06-04 22:57:50 +0700

    first commit

---
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            
+            $table->string('username');
+            $table->string('fullName');
+            $table->string('NIK');
+            $table->string('phone');
+            $table->string('email');
+            $table->string('address')->nullable();
+            $table->string('avatar')->nullable();
+            $table->enum('role', ['user', 'admin']);
+            $table->string('password');
+
+            $table->boolean('isEmailVerified');
+            $table->boolean('isNIKVerified');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('users');
+    }
+};
